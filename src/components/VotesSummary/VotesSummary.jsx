d28f7b816e44633ce0e9e02bf0cb0b879bd9d838
xavier <samuelpedroso08@gmail.com>
2024-11-19 10:00:01 -0300

    subindo alteração

--- src/components/VotesSummary/VotesSummary.jsx
+++ src/components/VotesSummary/VotesSummary.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import styles from './CandidateList.module.css'
+import styles from './VotesSummary.module.css'
 
 const VotesSummary = ({candidates}) => {
     return (
-        <div className={styles.allTasks}>
+        <div className={styles.summary}>
 
-            <p>
+            <p className={styles.totalVotes}>
                 Votos Totais: {candidates.reduce((total, candidate) => total + candidate.votes, 0)}
             </p>
 
-            <p>
+            <p className={styles.winner}>
                 Candidato Vencedor: {candidates.reduce((winner, candidate) => candidate.votes > winner.votes ? candidate : winner, candidates[0]).name}
             </p>
 
